fe0fde387bfafed77c10c5f93398a3755750f425
Ravi Prakash <ravip140891@gmail.com>
2023-01-06 16:27:18 +0530

    dropdown menu till women footwear created

--- homepage.js
+++ homepage.js
@@ -1,6 +1,9 @@
 const slider = document.querySelector('.slider_image')
 const nextBtn = document.querySelector('.next')
 const prevBtn = document.querySelector('.prev')
+const bottomWear = document.querySelector('.bottom-wear')
+const dropMenu = document.querySelectorAll("more_dropdown_list");
+const bottomWearMenu = document.querySelector(".bottom-wear-menu");
 let translate = 0;
 
 setInterval(() => {
@@ -28,3 +31,9 @@ prevBtn.addEventListener("click", () => {
     slider.style.transform = `translateX(${-translate}%)`;
   }
 });
+
+bottomWear.addEventListener('mouseover', () => {
+    dropMenu.forEach((item) => item.style.display = 'none')
+    bottomWearMenu.style.display = 'block'
+    console.log("hi");
+})
